b027c27b50535798a58b3d903d514ecd98c12266
AnaFern2023 <AnaFern2023@gmail.com>
2023-02-27 16:40:16 +0100

    ich_glaube_fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // console.log("Hello");
 
-const confirmPage = document.querySelector('footer section form[type="email"]')
+const confirmPage = document.querySelector('footer section form input[type="email"]')
 const deleteForPopUp = document.getElementById("deleteForPopUp")
 const addForPopUp = document.getElementById("addForPopUp")
 
@@ -9,6 +9,7 @@ function subscribe() {
     // popUp.classList.add("popUp");
     deleteForPopUp.style.display = "none";
     addForPopUp.style.display = "block"
+    console.log("Your Email is " + confirmPage.value)
 }
 
 function backToWebsite() {
